af8f14f2314e90f2421ffddf6981462eca797ced
datomo <dave.lengw@gmail.com>
2025-07-23 23:00:07 +0100

    added timeout to connect

--- src/connection/client.rs
+++ src/connection/client.rs
@@ -1,4 +1,6 @@
-use std::net::TcpStream;
+use std::net::{SocketAddr, TcpListener, TcpStream};
+use std::str::FromStr;
+use std::time::Duration;
 use crate::connection::Connection;
 
 pub struct Client{
@@ -9,7 +11,13 @@ pub struct Client{
 
 impl Client {
     pub fn new(host: &str, port: u16) -> Self {
-        Client{ host: host.to_string(), port }
+        Client{ host: host.to_string(), port}
+    }
+
+    pub fn connect_timeout(&self, duration: Duration) -> Result<Connection, String> {
+        let addr = SocketAddr::from_str(&format!("{}:{}", self.host.clone(), self.port)).map_err(|_| "Invalid address".to_string())?;
+        let stream = TcpStream::connect_timeout(&addr, duration).unwrap();
+        Connection::new(&self.host, self.port, stream)
     }
 
     pub fn connect(&self) -> Result<Connection, String> {
